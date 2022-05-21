630d6eda6382bb47eeede543b58bba2abbfbc136
Hamed Osama <hamedosama19@gmail.com>
2022-05-21 21:17:49 +0200

    edit home

--- Student_Affairs_Website/all_students/models.py
+++ Student_Affairs_Website/all_students/models.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.db import models
 
 class Students(models.Model):
+    # serial = models.Index("a")
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=48)
     gpa = models.IntegerField()
@@ -11,3 +13,8 @@ class Students(models.Model):
     email = models.EmailField(max_length=80)
     date_of_birth = models.DateField()
     mobile_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return str(self.id)
+    class Meta:
+        verbose_name = 'Student'
--- Student_Affairs_Website/all_students/views.py
+++ Student_Affairs_Website/all_students/views.py
@@ -6,5 +6,6 @@ from .models import Students
 def index(request):
     students = Students.objects.order_by('id')
     pass_db = {'students':students}
+    print(students[2])
     return render(request,'all_students/index.html',context=pass_db)
     # return HttpResponse("hello world")
